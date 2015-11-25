32f515b1be9f15232d88ada6cb52c0602a5c780c
rixivcf <niauxricardo@gmail.com>
2015-11-25 10:24:42 +0100

    isertion

---
+++ TriRechercheTest.java
@@ -0,0 +1,35 @@
+import junit.framework.TestCase;
+
+
+public class TriRechercheTest extends TestCase {
+
+	public void testInitTab() {
+		
+		
+	}
+
+	public void testAfficheTab() {
+		fail("Not yet implemented");
+	}
+
+	public void testEchangerValeurs() {
+		fail("Not yet implemented");
+	}
+
+	public void testTriBullesCroissant() {
+		fail("Not yet implemented");
+	}
+
+	public void testTriBullesDecroissant() {
+		fail("Not yet implemented");
+	}
+
+	public void testRechercher() {
+		fail("Not yet implemented");
+	}
+
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
